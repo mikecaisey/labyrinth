dfdbb7056c6640ef0bc9d32a310eec896ac317f4
Mike Caisey <mike.caisey@gmail.com>
2015-02-23 22:23:17 +0000

    Added shuffle for randomising games tiles

--- main/js/main.js
+++ main/js/main.js
@@ -39,11 +39,16 @@ define(['Tile'], function(Tile){
             44,46,48
         ] // 33
 
-        var gameTiles = []
-        for (var i = 0; i < 15; i++) {
-            gameTiles.push(new Tile())
-        }
+        var randomOrder = shuffleArray(gameSpaces)
 
+    }
+
+    var shuffleArray = function(a) { // Fisher-Yates shuffle, no side effects
+        var i = a.length,
+            t, j;
+        a = a.slice();
+        while (--i) t = a[i], a[i] = a[j = ~~ (Math.random() * (i + 1))], a[j] = t;
+        return a;
     }
 
     return Main
